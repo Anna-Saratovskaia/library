8f1d80f89c4c635105a9d2c60e83e78a5260fd5f
Anna-Saratovskaia <94197450+Anna-Saratovskaia@users.noreply.github.com>
2023-12-05 17:29:01 -0800

    Initial logic

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const myLibrary = [
-  {
-    title: "Kill a Mockingbird",
-    author: "Harper Lee",
-    year: 1960,
-    status: "read",
-  },
-  {
-    title: "1984",
-    author: "George Orwell",
-    year: 1949,
-    status: "read",
-  },
-];
+const myLibrary = [];
 
 function Book(title, author, year, status) {
   this.title = title;
@@ -20,11 +7,61 @@ function Book(title, author, year, status) {
   this.status = status;
 }
 
-let bookContainer = document.querySelector('tbody');
-
-
-function addBookToLibrary(myLibrary) {
-    for(let book of myLibrary ) {
-
-    }
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  return myLibrary;
 }
+
+let newBook1 = new Book("1984", "George Orwell", 1949, "Read");
+
+let newBook2 = new Book("Demons", "Fyodor Dostoevsky", 1872, "Read");
+
+addBookToLibrary(newBook1);
+addBookToLibrary(newBook2);
+
+const tbody = document.querySelector("tbody");
+
+function displayBook(myLibrary) {
+  
+  for (let i = 0; i < myLibrary.length; i++) {
+    const newTr = document.createElement("tr");
+    const tdAuthor = document.createElement("td");
+    const tdTitle = document.createElement("td");
+    const tdYear = document.createElement("td");
+    const tdStatus = document.createElement("td");
+    const removeBtn = document.createElement('button');
+    const changeStatus = document.createElement('button')
+    tbody.appendChild(newTr);
+    tdAuthor.textContent = myLibrary[i].author;
+    tdTitle.textContent = myLibrary[i].title;
+    tdYear.textContent = myLibrary[i].year;
+    tdStatus.textContent = myLibrary[i].status;
+    newTr.appendChild(tdTitle);
+    newTr.appendChild(tdAuthor);
+    newTr.appendChild(tdYear);
+    newTr.appendChild(tdStatus);
+    newTr.appendChild(removeBtn);
+    newTr.appendChild(changeStatus);
+  }
+}
+
+ const dialog = document.querySelector('dialog');
+ const newBookBtn = document.querySelector('.new-book');
+ const addBtn = document.querySelector('.add')
+
+ newBookBtn.addEventListener('click', function() {
+  dialog.showModal()
+ })
+
+ addBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  dialog.close();
+  let title = document.querySelector('[name="title"]');
+  let author = document.querySelector('[name="author"]')
+  let year = document.querySelector('[name="year"]')
+  let status = document.querySelector('[name="status"]')
+
+  let newBook = new Book(title.value, author.value, year.value, status.value );
+  addBookToLibrary(newBook)
+  displayBook(myLibrary)
+ })
